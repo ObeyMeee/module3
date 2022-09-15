aa5663e8e0c9360f2d914718b25611d8c2226a9a
Andrrr <geraskin19970309@gmail.com>
2022-09-15 10:58:50 +0300

    update database config for heroku

--- src/main/java/ua/com/andromeda/config/FlywayConfig.java
+++ src/main/java/ua/com/andromeda/config/FlywayConfig.java
@@ -5,9 +5,10 @@ import org.flywaydb.core.Flyway;
 
 @Getter
 public class FlywayConfig {
-    private static final String URL = "jdbc:postgresql://localhost:5432/university";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "1337";
+    private static final String URL = "jdbc:postgresql://ec2-52-200-5-135.compute-1.amazonaws.com/df9o8qhhhdfp63";
+    private static final String USERNAME = "gyxtucboengfyq";
+
+    private static final String PASSWORD = "2476f1e2b7474f8e2b440b755a82ade63eb7421306ea7d5e61c328da5ffd6976";
     private static FlywayConfig instance;
     private final Flyway flyway;
 
